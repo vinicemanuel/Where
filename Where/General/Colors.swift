0d61634d39ca12936dc42932fd8b4a9bf0c82354
Vinicius Silva <viniciussilva@BARCELOS-008756.Dlink>
2021-11-08 21:18:28 -0300

    base for record view

---
+++ Where/General/Colors.swift
@@ -0,0 +1,15 @@
+//
+//  Colors.swift
+//  Where
+//
+//  Created by Vinicius Silva on 08/11/21.
+//
+
+import Foundation
+import UIKit
+
+struct Colors {
+    static let background = UIColor(named: "Backgroud")!
+    static let icons = UIColor(named: "Icons")!
+    static let texts = UIColor(named: "Texts")!
+}
---
+++ Where/Record/RecordViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RecordViewController.swift
+//  Where
+//
+//  Created by Vinicius Silva on 08/11/21.
+//
+
+import UIKit
+import MapKit
+
+class RecordViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
